6ded8f8409a1cc3a3f3c28c530f9bfab2e5223db
santiellis <Santiagoellis@hotmail.com.ar>
2022-09-27 17:54:15 -0300

    Database name

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,API_KEY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
